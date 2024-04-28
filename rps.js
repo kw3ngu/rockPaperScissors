3640fc42581cb1f6cee4efea76900cbe8bf380d9
kw3ngu <kwengu72@gmail.com>
2024-04-28 11:55:39 +0530

    play Game function completed

--- rps.js
+++ rps.js
@@ -8,47 +8,53 @@ function getHumanChoice(){
     return x;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
 
+        function playRound(humanChoice, computerChoice){
+            humanChoice = getHumanChoice();
+            computerChoice = getComputerChoice();
+            console.log(`You choose => ${humanChoice}`);
+            console.log(`Computer chooses => ${computerChoice}`);
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    console.log(`You choose => ${humanChoice}`);
-    console.log(`Computer chooses => ${computerChoice}`);
+            let result = "" 
 
-    let result = "" 
+            if(humanChoice === computerChoice){
+                result = "DRAWWWW"
+            }
+            else{
+                switch(humanChoice){
+                    case "rock":
+                        result = (computerChoice === "scissors") ? "YOU WIN" : "LOSERRR";
+                        break;
+                    case "paper":
+                        result = (computerChoice === "rock") ? "YOU WIN" : "LOSERRR";
+                        break;
+                    case "scissors":
+                        result = (computerChoice === "paper") ? "YOU WIN" : "LOSERRR";
+                        break;
+                }
+            }
+            console.log(result)
 
-    if(humanChoice === computerChoice){
-        result = "DRAWWWW"
-    }
-    else{
-        switch(humanChoice){
-            case "rock":
-                result = (computerChoice === "scissors") ? "YOU WIN" : "LOSERRR";
-                break;
-            case "paper":
-                result = (computerChoice === "rock") ? "YOU WIN" : "LOSERRR";
-                break;
-            case "scissors":
-                result = (computerChoice === "paper") ? "YOU WIN" : "LOSERRR";
-                break;
+            switch(result){
+                case "YOU WIN":
+                    humanScore++;
+                    break;
+                case "LOSERRR":
+                    computerScore++;
+                    break;
+                case "DRAWWWW":
+                    break;
+            }
         }
-    }
-    console.log(result)
-    switch(result){
-        case "YOU WIN":
-            humanScore++;
-            break;
-        case "LOSERRR":
-            computerScore++;
-            break;
-    }
 
-    console.log(`YOUR SCORE: ${humanScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
+    for(let round = 1; round <= 10; round++){
+        playRound();
+        console.log(`YOUR SCORE => ${humanScore}`);
+        console.log(`COMPUTER SCORE => ${computerScore}`);
+    }
 }
-playRound()
 
-
+playGame()
