3444fb59f1e47690df64afc4de13a08ebf32d69e
kw3ngu <kwengu72@gmail.com>
2024-05-09 21:57:49 +0530

    final js

--- rps.js
+++ rps.js
@@ -1,68 +1,78 @@
-function getComputerChoice(){
+let userScore = 0;
+let computerScore = 0;
+const choices_div = document.querySelector(".choices")
+const rockPara = document.querySelector("#rock");
+const paperPara = document.querySelector("#paper");
+const scissorsPara = document.querySelector("#scissors");
+const playerPara = document.querySelector("#player")
+const compPara = document.querySelector("#computer")
+const resultPara = document.querySelector(".result");
+const computerDisplay = document.querySelector("#compDisplay");
+
+
+
+
+rockPara.addEventListener('click', () => {
+    playGame("rock");
+})
+
+paperPara.addEventListener('click', () => {
+    playGame("paper");
+})
+
+scissorsPara.addEventListener('click', () => {
+    playGame("scissor");
+})
+
+
+
+function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
-    const randomNum = Math.floor(Math.random() * 3)
+    const randomNum = Math.floor(Math.random() * 3);
     return choices[randomNum];
 }
-function getHumanChoice(){
-    const x = prompt("Enter your choice").toLowerCase();
-    return x;
-}
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playGame(userChoice) {
+    let result = "";
+    const computerChoice = getComputerChoice();
+    computerDisplay.textContent = `Computer chose ${computerChoice}`;
 
-        function playRound(humanChoice, computerChoice){
-            humanChoice = getHumanChoice();
-            computerChoice = getComputerChoice();
-            console.log(`You choose => ${humanChoice}`);
-            console.log(`Computer chooses => ${computerChoice}`);
+    if(userChoice === computerChoice){
+        result = "ITS A DRAW";
+    }else {
+        switch(userChoice){
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN" : "YOU LOSE";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN" : "YOU LOSE";
+                break;
+            case "scissor":
+                result = (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE";
+                break;
+        }
+    }
+    resultPara.textContent = result;
 
-            let result = "" 
 
-            if(humanChoice === computerChoice){
-                result = "DRAWWWW"
-            }
-            else{
-                switch(humanChoice){
-                    case "rock":
-                        result = (computerChoice === "scissors") ? "YOU WIN" : "LOSERRR";
-                        break;
-                    case "paper":
-                        result = (computerChoice === "rock") ? "YOU WIN" : "LOSERRR";
-                        break;
-                    case "scissors":
-                        result = (computerChoice === "paper") ? "YOU WIN" : "LOSERRR";
-                        break;
-                }
-            }
-            console.log(result)
-
-            switch(result){
-                case "YOU WIN":
-                    humanScore++;
-                    break;
-                case "LOSERRR":
-                    computerScore++;
-                    break;
-                case "DRAWWWW":
-                    break;
-            }
+        switch(result){
+            case "YOU WIN":
+                resultPara.classList.add("greentext");
+                userScore++;
+                playerPara.textContent = userScore;
+                break;
+            case "YOU LOSE":
+                resultPara.classList.add("redtext");
+                computerScore++;
+                compPara.textContent = computerScore;
+                break;
+            case "ITS A DRAW":
+                resultPara.classList.remove("greentext", "redtext");
+                userScore;
+                computerScore;
+                break;
         }
 
-    for(let round = 1; round <= 5; round++){
-        playRound();
-        console.log(`YOUR SCORE => ${humanScore}`);
-        console.log(`COMPUTER SCORE => ${computerScore}`);
-    }
-    while(round = 5){
-        if(humanScore === computerScore){
-            console.log("ITS A DRAW");
-        }else if(humanScore > computerScore){
-            console.log("YOU ARE THE WINNER");
-        }else console.log("HAHAHA TRY AGAIN");
-        break;
-    }
 }
 
-playGame()
+
